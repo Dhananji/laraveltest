b20dea50b5332a2f4f4d3dd7379e24b110cf597a
Nushan <nushan@paymedia.lk>
2020-01-29 17:26:16 +0530

    change

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book\Book;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
+use Illuminate\Support\Facades\View;
 
 class BookController extends Controller
 {
